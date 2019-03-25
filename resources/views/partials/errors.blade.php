25d98158dc05580fde79ea0939b912e12258c49e
lajtof <thel4jtowy@gmail.com>
2019-03-25 21:23:16 +0100

    added flex to alerts

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,5 +1,5 @@
 @if ($errors->any())
-    <div class="alert alert-danger alert-dismissible fade show mx-2 my-1 shadow position-fixed w-25 lajtof-alert">
+    <div class="alert alert-danger alert-dismissible fade show mx-2 my-1 shadow position-fixed d-flex lajtof-alert">
         @foreach ($errors->all() as $error)
             <span>{{ $error }}</span>
         @endforeach
@@ -8,7 +8,7 @@
 @endif
 
 @if (session('error'))
-    <div class="alert alert-danger alert-dismissible fade show mx-2 my-1 shadow position-fixed w-25 lajtof-alert">
+    <div class="alert alert-danger alert-dismissible fade show mx-2 my-1 shadow position-fixed d-flex lajtof-alert">
         {{ session('error') }}
         <button type="button" class="close" data-dismiss="alert">&times;</button>
     </div>
--- resources/views/partials/success.blade.php
+++ resources/views/partials/success.blade.php
@@ -1,5 +1,5 @@
 @if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show mx-2 my-1 shadow position-fixed w-25 lajtof-alert">
+    <div class="alert alert-success alert-dismissible fade show mx-2 my-1 shadow position-fixed d-flex lajtof-alert">
         {{ session('success') }}
         <button type="button" class="close" data-dismiss="alert">&times;</button>
     </div>
